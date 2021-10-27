3444ce37674b724c9e77653df8493682eedc4e20
antonyandrus <antonyandrews07@icloud.com>
2021-10-27 22:08:15 +0530

    Feature: Download CV

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { jsPDF } from 'jspdf'
+import html2canvas from 'html2canvas';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'profilecv';
+
+  captureScreen() {
+    let data = document.getElementById('contentToConvert');
+    html2canvas(data as any).then(canvas => {
+      var imgWidth = 210;
+      var pageHeight = 295;
+      var imgHeight = canvas.height * imgWidth / canvas.width;
+      var heightLeft = imgHeight;
+      const contentDataURL = canvas.toDataURL('image/png');
+      let pdfData = new jsPDF('p', 'mm', 'a4');
+      var position = 0;
+      pdfData.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+      pdfData.save(`resume.pdf`);
+    });
+  }
+}
