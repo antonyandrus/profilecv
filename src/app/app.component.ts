2b6abb13c5b430df92bdcb298432e169bc04c34c
antonyandrus <antonyandrews07@icloud.com>
2021-10-28 13:52:32 +0530

    Feature: Profile Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { jsPDF } from 'jspdf'
-import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +8,4 @@ import html2canvas from 'html2canvas';
 export class AppComponent {
   title = 'profilecv';
 
-  captureScreen() {
-    let data = document.getElementById('contentToConvert');
-    html2canvas(data as any).then(canvas => {
-      var imgWidth = 210;
-      var pageHeight = 295;
-      var imgHeight = canvas.height * imgWidth / canvas.width;
-      var heightLeft = imgHeight;
-      const contentDataURL = canvas.toDataURL('image/png');
-      let pdfData = new jsPDF('p', 'mm', 'a4');
-      var position = 0;
-      pdfData.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
-      pdfData.save(`resume.pdf`);
-    });
-  }
 }
